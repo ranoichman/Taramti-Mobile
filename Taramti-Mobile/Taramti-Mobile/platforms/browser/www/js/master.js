e2ae4bee38dcdb16c667eaa325bd2e7164edc001
ranoichman <ran.oichman@gmail.com>
2017-05-07 17:48:00 +0300

    הווב סרוויסים עובדים בפונגאפ

---
+++ Taramti-Mobile/Taramti-Mobile/platforms/browser/www/js/master.js
@@ -0,0 +1,16 @@
+﻿
+const GENERAL = {
+
+    genericWebServerAddress: "/WebService.asmx/",
+    //webServerAddress: "http://proj.ruppin.ac.il/bgroup51/test2/webservice.asmx/",
+
+    assocWebServerAddress: "/WebService.asmx/",
+    //assocWebServerAddress:"http://proj.ruppin.ac.il/bgroup51/test2/webservice.asmx/",
+
+    
+
+}
+
+
+
+
