dc5d9ee7d1213dacbb79916f6a85f40c74a9ab65
ranoichman <ran.oichman@gmail.com>
2017-05-07 18:29:58 +0300

    שמות משתנים מהמאסטר ג'אווה סקריפט

--- Taramti-Mobile/Taramti-Mobile/platforms/browser/www/js/master.js
+++ Taramti-Mobile/Taramti-Mobile/platforms/browser/www/js/master.js
@@ -4,8 +4,8 @@ const GENERAL = {
     genericWebServerAddress: "/WebService.asmx/",
     //webServerAddress: "http://proj.ruppin.ac.il/bgroup51/test2/webservice.asmx/",
 
-    assocWebServerAddress: "/WebService.asmx/",
-    //assocWebServerAddress:"http://proj.ruppin.ac.il/bgroup51/test2/webservice.asmx/",
+    assocWebServerAddress: "/../AssociationsWebService.asmx/",
+    //assocWebServerAddress:"http://proj.ruppin.ac.il/bgroup51/test2/AssociationsWebService.asmx/",
 
     
 
